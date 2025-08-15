2ffd9977bac3b48a419fd3b399eacadbf048d195
kv <kvdata9@gmail.com>
2025-08-15 15:02:50 +0530

    Update project with latest changes

--- app/routes.ts
+++ app/routes.ts
@@ -5,7 +5,7 @@ export default [
     route('api/create-trip', 'routes/api/create-trip.ts'),
     layout('routes/admin/admin-layout.tsx',
         [
-            route(index, 'routes/admin/dashboard.tsx'),
+            route('/', 'routes/admin/dashboard.tsx'),
             route("/all-users", 'routes/admin/all-users.tsx'),
             route("trips" , 'routes/admin/trips.tsx'),
             route("trips/create", 'routes/admin/create-trip.tsx'),
